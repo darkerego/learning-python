ee36139b67c1d4836b2e0b4a9490f6cb77ff8840
Darkerego <darkerego@users.noreply.github.com>
2015-10-07 11:50:33 -0400

    Create filename-huh.py
    No, this program does not do anything useful.

---
+++ filename-huh.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# Chev Y. [Sept 17, '15]
+# URI PATH ... Formation?
+##########################
+import re
+
+def get_FILE():
+    file = input("What is the name of the file? :")
+
+    if not re.match(r'^[a-z-A-Z-0-9]*$', file):
+                raise ValueError('')
+    else:
+        return file
+
+def get_EXT():
+    ext = input("What is the file extension? (Enter with the dot '.') :")
+    if not re.match(r'[.[a-z-A-Z-0-9]*$', ext):
+                    raise ValueError('')
+    else:
+        return ext
+def get_URI():
+
+    path = input ("Where is the file located (No trailing slash please) :")
+    if not re.match (r'[\/[a-z-A-Z-0-9]*]*$', path):
+                    raise ValueError('')
+    else:
+        return path
+
+while True:
+    try:
+        file = get_FILE()
+        break
+    except ValueError:('')
+
+while True:
+    try:
+        ext = get_EXT()
+        break
+    except ValueError:('')
+
+while True:
+    try:
+        path = get_URI()
+        break
+    except ValueError:('')
+
+
+print ((path)+"/"+(file)+(ext))
+
+input("Press Enter to Quit...")

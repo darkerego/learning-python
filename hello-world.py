d2a2de16123a40b6ecd314f52b53ead42cf69079
Darkerego <darkerego@users.noreply.github.com>
2015-10-07 11:45:39 -0400

    Create hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python#
+##################
+# Hello World   ##
+##################
+# Author DarkerEgo
+# Written 9/3/15
+# HelloWorld: Print 'Hello World' and the date to stout.
+
+import time # call the datetime library
+
+print("Hello World...") # print to stio
+print ("\n")                # new line
+print ("...............")
+print (time.strftime("%B %d")) # print variables from date
+                                # (month + decimal day)
+
